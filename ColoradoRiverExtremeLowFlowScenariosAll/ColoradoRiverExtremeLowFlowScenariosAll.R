02aeb0eda2bb633272eda5384edd33d8838f2b25
dzeke <david.rosenberg@usu.edu>
2025-08-05 11:47:23 -0600

    Finished plot with legend on top

--- ColoradoRiverExtremeLowFlowScenariosAll/ColoradoRiverExtremeLowFlowScenariosAll.R
+++ ColoradoRiverExtremeLowFlowScenariosAll/ColoradoRiverExtremeLowFlowScenariosAll.R
@@ -86,12 +86,14 @@ cRedsToPlot <- brewer.pal(9, "Reds")
 
 #### Figure 1 - Lines as 
 
-ggplot(data = dfExtremeFlows, aes(x = `Minimum (maf)`, y = Order, xend = `Maximum (maf)`, fill = TypeLocation, 
+ggplot(data = dfExtremeFlows, aes(x = `Minimum (maf)`, y = Order, xend = `Maximum (maf)`, color = TypeLocation, 
                                   yend = Order )) +
   geom_segment(size = 10) +
   
   #scale_linetype_manual(values = c("solid","longdash")) +
   
+  scale_color_manual( values = c("Lee Ferry-Natural" = cBluesToPlot[8], "Lake Powell-Regulated Inflow" = cBluesToPlot[6], "Lake Powell-Release" = cBluesToPlot[5], "Lake Mead-Regulated Inflow" = cRedsToPlot[7]),
+                        breaks = c(dfExtremeFlows$TypeLocation[1], dfExtremeFlows$TypeLocation[3], dfExtremeFlows$TypeLocation[4], dfExtremeFlows$TypeLocation[5])) +
   #scale_color_manual(values = cColorsToPlot) +
   # Set the x and y axis limits, breaks. Left Y axis for flow method and secondary right Y axis for Stabilize strategy
   scale_x_continuous(limits = c(2,14), breaks = seq(2,14,2), minor_breaks = seq(2,14,2), sec.axis = sec_axis(~ . * 1, breaks = seq(2,14,2))) +
@@ -109,7 +111,10 @@ ggplot(data = dfExtremeFlows, aes(x = `Minimum (maf)`, y = Order, xend = `Maximu
   #labs(x="", y="") +
   #theme(text = element_text(size=20), legend.title=element_blank(), legend.text=element_text(size=18),
   #      legend.position = c(0.8,0.7))
-  theme(text = element_text(size=16), legend.title = element_text("Annual Release\nMAF"), legend.text=element_text(size=14), axis.text.x = element_text(size=12))
+  theme(text = element_text(size=16),  legend.title=element_blank(), 
+        legend.text=element_text(size=14), axis.text.x = element_text(size=12),
+        panel.grid.minor = element_blank(),
+        legend.position = "top")
 
   
 
