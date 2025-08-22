d3ad5e1cea623981f97882c3d97b0dcffdf22a53
dzeke <david.rosenberg@usu.edu>
2025-08-22 17:04:53 -0600

    Figured out site locations

--- LakePowellTempElevMarginal/PowellTemperatureProfiles.R
+++ LakePowellTempElevMarginal/PowellTemperatureProfiles.R
@@ -3,7 +3,7 @@
 #
 # This script does 3 things:
 #
-#   1. Injests temperature profile data from the USGS Lake Powell profiles: 
+#   1. Ingests temperature profile data from the USGS Lake Powell profiles: 
 #         Andrews, C.M., and Deemer, B.R., 2022, Limnology data from Lake Powell, desert southwest USA (ver. 2.1, September 2024): U.S. Geological Survey data release, https://doi.org/10.5066/P9ZIKVYW. 
 #       https://www.sciencebase.gov/catalog/item/62b39805d34e8f4977cb788c
 #       
@@ -11,18 +11,18 @@
 #              SiteVisitData_LakePowell_1964-2022.csv (Site visit meta data, including Lake Elevation and Intake Depth)
 #              ProfileData_LakePOwell_1965-2022.csv (Temperature-Depth data)
 #
-#   2. Lake Powell Bathymetry (2017) from Bradley, D., and Collins, K. (2022). "Lake Powell 2017 Area and Capacity Tables." ENV-2021-98, Reclamation. https://doi.org/10.5066/P9O3IPG3.
+#   2. Ingest Lake Powell Bathymetry (2017) from Bradley, D., and Collins, K. (2022). "Lake Powell 2017 Area and Capacity Tables." ENV-2021-98, Reclamation. https://doi.org/10.5066/P9O3IPG3.
 
 #        File: Lake_Powell_Area_Capacity_Table_report_Final.xlsx
 #       Note: this excel file was created from the raw data published in pdf form. See here for more specifics:
 #         https://github.com/dzeke/ColoradoRiverCollaborate/tree/main/LakePowellNewBathymetry
 #        
-#   3. Join all the above files together to get a single data frame from which we will perform the analysis.
+#   3. Joins all the above files together to get a single data frame from which we will perform the analysis.
 #
 #   https://www.usbr.gov/pn-bin/hdb/hdb.pl?svr=uchdb2&sdi=1862%2C1872%2C4167%2C4166&tstp=HR&t1=2024-05-01T00:00&t2=2025-07-10T00:00&table=R&mrid=0&format=csv
 #
 # David Rosenberg
-# August 14, 2025
+# August 22, 2025
 
 #Versions to use
 #R version 4.1.1. Download from https://cran.r-project.org/.
@@ -31,7 +31,7 @@
 # Remove everything
 rm(list=ls())
 
-cPackages <- c("versions", "googlesheets4", "dygraphs", "tidyquant", "xts", "tidyverse", "tidyquant","lubridate", "stringr", "rvest", "RColorBrewer", "readxl" )
+cPackages <- c("versions", "googlesheets4", "dygraphs", "tidyquant", "xts", "tidyverse", "tidyquant","lubridate", "stringr", "rvest", "RColorBrewer", "readxl", "ggmap" )
 
 # Install packages not yet installed
 installed_packages <- cPackages %in% rownames(installed.packages())
@@ -55,6 +55,10 @@ interp2 <- function(x, y, xi = x, ...) {
   yi[sel] <- interp1(x = x, y = y, xi = xi[sel], ...);
   return(yi);
 }
+
+### Stations we are interested in:
+sStations <- c("LPCRRFG2", "LPCR0004", "LPCR0006", "LPCR0024", "LPCR-0001")
+
 
 #######
 ###Read all data data into R
@@ -85,33 +89,32 @@ dfProfileJoined$LakeElevation_ft <- 3.28 * dfProfileJoined$LakeElevation_m
 dfProfileJoined$BottomSounding_ft <- 3.28 * dfProfileJoined$BottomSounding_m
 dfProfileJoined$IntakeDepth_ft <- 3.28 * dfProfileJoined$IntakeDepth_m
 
+# Convert Date to POSIXct format R understands
+dfProfileJoined$PosDate <- as.POSIXct(dfProfileJoined$CollectionDateTime.x, format = "%m/%d/%Y %H:%M")
+
+# Calculate the year
+dfProfileJoined$Year <- year(dfProfileJoined$PosDate)
+
+# Calculate the intake elevation in feet to check that elevations are all the same
+dfProfileJoined$IntakeElevationMid_ft <- dfProfileJoined$LakeElevation_ft - dfProfileJoined$IntakeDepth_ft
+dfProfileJoined$MinimumPowerPool_ft <- 3490
+
+dfProfileSummary <- dfProfileJoined %>% group_by(StationID, Description) %>% summarize(MinYear = min(Year), MaxYear = max(Year), Latitude = mean(Latitude), Longitude = mean(Longitude))
+
+# Find the unique stations
+cUniqueStats <- unique(dfProfileJoined$StationID)
+
+#Filter on the stations we are interested in
+dfProfileJoinedStats <- dfProfileJoined %>% filter(StationID %in% sStations)
+
+# Summarize years of data and locations for those points
+dfProfileSummary <- dfProfileJoinedStats %>% group_by(StationID, Description) %>% summarize(MinYear = min(Year), MaxYear = max(Year), Latitude = mean(Latitude), Longitude = mean(Longitude))
+
+##### Print a map of Lake Powell to see where the Locations are
+
+
 #Interpolate reservoir storage from lake elevation
 ###dfProfileJoined$ActiveVolume_acft <- interp2(xi = dfProfileJoined$LakeElevation_ft,x=dfMeadElevStor$`Elevation (ft)` , y=dfMeadElevStor$`Live Storage (ac-ft)`, method="linear")
 
 
-#Turn to meaningful column names
-cColNames <- colnames(dfReleases)
-cColNames[2:5] <- c("PowerRelease", "TotalRelease", "BypassRelease", "SpillwayRelease")
-colnames(dfReleases) <- cColNames 
 
-#Save to csv
-write.csv(dfReleases, "LakePowellReleases.csv")
-
-# Convert API Date Time to POSIXct
-dfReleases$DateTimePos <- as.POSIXct(as.character(dfReleases$DATETIME), format = "%m/%d/%Y %H:%M")
-
-
-# Replace NaNs with NA in all columns
-dfReleases <- na.omit(dfReleases)
-
-#Convert to xts
-dfXtsRelease <- xts(cbind(dfReleases$PowerRelease, dfReleases$BypassRelease, dfReleases$SpillwayRelease, dfReleases$TotalRelease), order.by=dfReleases$DateTimePos)
-
-#Plot the Release dygraph
-dygraph(dfXtsRelease, ylab = "Release (cfs)") %>% dyRangeSelector() %>%
-  dySeries("V1", label = "PowerRelease") %>%
-  dySeries("V2", label = "BypassRelease") %>%
-  dySeries("V3", label = "SpillwayRelease") %>%
-  dySeries("V4", label = "TotalRelease") # %>%
-# #  dySeries("V5", label = "Outside") %>%
-#dyLimit(32, color = "red")
